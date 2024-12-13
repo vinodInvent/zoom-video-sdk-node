7ecb8d4507fdca784064568f24d32dc07b30d8de
gummy <vinod.saini@inventcolab.com>
2024-12-13 11:41:35 +0530

    x

--- ecosystem.config.mjs
+++ ecosystem.config.mjs
@@ -1,18 +1,16 @@
-
-module.exports = {
+export default {
     apps: [
       {
-        name: "zoom-video-sdk", // Application name
-        script: "npm",       // Command to run
-        args: "start",       // Arguments for the script
+        name: "zoom-video-sdk-1", // Application name
+        script: "npm",                         // Run the npm command
+        args: "start",                         // Use the "start" script defined in package.json
         env: {
-          NODE_ENV: "development", // Environment for development
+          NODE_ENV: "development",            // Development environment
         },
         env_production: {
-          NODE_ENV: "production", // Environment for production
+          NODE_ENV: "production",             // Production environment
         },
       },
     ],
   };
-
   
